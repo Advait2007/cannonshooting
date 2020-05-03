57653cacb566448faf80154e587eed7a1d9ac938
Advait2007 <64078214+Advait2007@users.noreply.github.com>
2020-05-03 20:53:26 +0530

    Added Assets and JS

---
+++ js/Tanker.js
@@ -0,0 +1,10 @@
+class Tanker extends BaseClass{
+  constructor(x,y){
+    super(400,50,150,150);
+    this.image = loadImage("assets/tanker.png");
+  }
+  display(){
+  
+    super.display();
+  }
+}
